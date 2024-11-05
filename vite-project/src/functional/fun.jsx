06c0074080bcd83ef93edd3b2f8afacf5171d792
MCHAITANYANAIDU <mchaitanyaprasad65@gmail.com>
2024-11-05 15:45:31 +0530

    new

--- vite-project/src/functional/fun.jsx
+++ vite-project/src/functional/fun.jsx
@@ -1,35 +1,35 @@
 
-// import { useState } from "react"
+import { useState } from "react"
 
 
 
-// function Hook(){
+function Hook(){
 
-//     var [a,seta]=useState(0);
-//     function env(){
-//         seta((ps)=>{
-//             console.log(ps);
+    var [a,seta]=useState(0);
+    function env(){
+        seta((ps)=>{
+            console.log(ps);
 
-//             return ps+1
+            return ps+1
             
-//         })
-//         // seta(a+1)
-//      }
+        })
+        // seta(a+1)
+     }
 
-//      var dec=()=>{
-//         seta(a-1)
+     var dec=()=>{
+        seta(a-1)
 
-//      }
+     }
 
-//     return (
-//         <>
-//         <h1>TODO</h1>
-//         <center><h2>{a}</h2></center>
-//         <button  onClick={env}>ADD</button>
-//         <button  onClick={dec}>SUB</button>
+    return (
+        <>
+        <h1>TODO</h1>
+        <center><h2>{a}</h2></center>
+        <button  onClick={env}>ADD</button>
+        <button  onClick={dec}>SUB</button>
 
-//         </>
-//     )
-// }
+        </>
+    )
+}
 
-// export default Hook;
+export default Hook;
